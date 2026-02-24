051c229c648ad9698ae563d8d164d90f42285555
CodeWander-666 <nikhilsinghp916@gmail.com>
2026-02-24 12:12:10 +0000

    Add Mesh2Motion source code

---
+++ src/lib/enums/SkeletonType.ts
@@ -0,0 +1,19 @@
+// Supported skeleton types
+// the file names for the skeleton types are associated with the const
+export enum SkeletonType {
+  Quadraped = 'rigs/rig-fox.glb',
+  Human = 'rigs/rig-human.glb',
+  Bird = 'rigs/rig-bird.glb',
+  Dragon = 'rigs/rig-dragon.glb',
+  Kaiju = 'rigs/rig-kaiju.glb',
+  Error = 'ERROR',
+  None = 'NONE'
+}
+
+// Hand skeleton variation types for human skeleton
+export enum HandSkeletonType {
+  AllFingers = 'all-fingers',
+  ThumbAndIndex = 'thumb-and-index',
+  SimplifiedHand = 'simplified-hand',
+  SingleBone = 'single-bone'
+}
